f9f56f3dc628b1a5606b1c938057fc951617064d
Joanjo <mtz230396@gmail.com>
2020-08-30 16:08:23 -0500

    add styles

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():    
-    return 'Hello World!'
-
+    return render_template('landing.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
