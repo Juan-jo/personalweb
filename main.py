151aed265c47b1572d74faaef0a4290d9d8833cb
Joanjo <mtz230396@gmail.com>
2020-09-04 12:13:19 -0500

    se añade datos de contacto

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():    
+def main():    
     return render_template('landing.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
